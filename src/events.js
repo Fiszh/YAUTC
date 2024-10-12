789eb91fe0be11df9b3dc41dde868480fcda5a26
Killa Fish <61081555+Fiszh@users.noreply.github.com>
2024-10-13 01:24:09 +0200

    Update events.js

--- src/events.js
+++ src/events.js
@@ -3,19 +3,21 @@ let pressedKeys = {};
 const avatar = document.querySelector('.user_avatar');
 const dropdown = document.getElementById('dropdown');
 
-document.querySelector('#followed').addEventListener('mouseover', () => {
-    document.querySelector('.chat').style.transition = 'width 0.3s ease';
-    document.querySelector('#twitch-embed').style.transition = 'height 0.3s ease';
-    document.querySelector('.chat').style.width = '31.7%';
-    document.querySelector('#twitch-embed').style.height = '79.8%';
-});
-
-document.querySelector('#followed').addEventListener('mouseout', () => {
-    document.querySelector('.chat').style.transition = 'width 0.3s ease';
-    document.querySelector('#twitch-embed').style.transition = 'height 0.3s ease';
-    document.querySelector('.chat').style.width = '30%';
-    document.querySelector('#twitch-embed').style.height = '85.8%';
-});
+if (document.querySelector('#followed')) {
+    document.querySelector('#followed').addEventListener('mouseover', () => {
+        document.querySelector('.chat').style.transition = 'width 0.3s ease';
+        document.querySelector('#twitch-embed').style.transition = 'height 0.3s ease';
+        document.querySelector('.chat').style.width = '31.7%';
+        document.querySelector('#twitch-embed').style.height = '79.8%';
+    });
+    
+    document.querySelector('#followed').addEventListener('mouseout', () => {
+        document.querySelector('.chat').style.transition = 'width 0.3s ease';
+        document.querySelector('#twitch-embed').style.transition = 'height 0.3s ease';
+        document.querySelector('.chat').style.width = '30%';
+        document.querySelector('#twitch-embed').style.height = '85.8%';
+    });
+}
 
 document.addEventListener('keydown', (event) => {
     pressedKeys[event.key] = true;
