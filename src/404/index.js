5b173f354fc28cb560faaa17c73a3f1cb417dd7a
Killa Fish <61081555+Fiszh@users.noreply.github.com>
2024-11-01 19:10:43 +0100

    404 fix
    i hope

--- src/404/index.js
+++ src/404/index.js
@@ -1,17 +1,11 @@
-if (path) {
-    document.location.reload()
-} else {
-    const path = document.location.pathname
-
-    function update() {
-        if (path !== '/YAUTC/' && !path.endsWith('/')) {
-            const infoElements = document.getElementsByClassName('information');
-            console.log(infoElements)
-            if (infoElements.length > 0) {
-                infoElements[0].innerHTML = "(404) <br> Recently, the format was changed from /channel_name to #/channel_name";
-            }
+function update() {
+    if (document.location.pathname !== '/YAUTC/' && !document.location.pathname.endsWith('/')) {
+        const infoElements = document.getElementsByClassName('information');
+        console.log(infoElements)
+        if (infoElements.length > 0) {
+            infoElements[0].innerHTML = "(404) <br> Recently, the format was changed from /channel_name to #/channel_name";
         }
     }
-
-    setInterval(update, 100);
 }
+
+setInterval(update, 100);
