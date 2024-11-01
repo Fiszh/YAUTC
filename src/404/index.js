e113e9daad97e22a288ade7056f6c2e8d517dcbc
Killa Fish <61081555+Fiszh@users.noreply.github.com>
2024-11-01 19:01:01 +0100

    404 error for wrong channel path should be fixed

--- src/404/index.js
+++ src/404/index.js
@@ -1,13 +1,11 @@
 const path = document.location.pathname
 
-console.log(path)
-console.log(path !== '/YAUTC/')
-console.log(!path.endsWith('/'))
-
-if (path !== '/YAUTC/' && !path.endsWith('/')) {
-    const infoElements = document.getElementsByClassName('information');
-    console.log(infoElements)
-    if (infoElements.length > 0) {
-        infoElements[0].innerHTML = "(404) <br> Recently, the format was changed from /channel_name to #/channel_name";
+setInterval(() => {
+    if (path !== '/YAUTC/' && !path.endsWith('/')) {
+        const infoElements = document.getElementsByClassName('information');
+        console.log(infoElements)
+        if (infoElements.length > 0) {
+            infoElements[0].innerHTML = "(404) <br> Recently, the format was changed from /channel_name to #/channel_name";
+        }
     }
-}
+}, 100);
