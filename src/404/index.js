c4a9167e5007328ba3f96e4bcde7052359bb7f9d
Killa Fish <61081555+Fiszh@users.noreply.github.com>
2024-11-01 19:09:16 +0100

    test

--- src/404/index.js
+++ src/404/index.js
@@ -1,13 +1,17 @@
-path = document.location.pathname
+if (path) {
+    document.location.reload()
+} else {
+    const path = document.location.pathname
 
-function update() {
-    if (path !== '/YAUTC/' && !path.endsWith('/')) {
-        const infoElements = document.getElementsByClassName('information');
-        console.log(infoElements)
-        if (infoElements.length > 0) {
-            infoElements[0].innerHTML = "(404) <br> Recently, the format was changed from /channel_name to #/channel_name";
+    function update() {
+        if (path !== '/YAUTC/' && !path.endsWith('/')) {
+            const infoElements = document.getElementsByClassName('information');
+            console.log(infoElements)
+            if (infoElements.length > 0) {
+                infoElements[0].innerHTML = "(404) <br> Recently, the format was changed from /channel_name to #/channel_name";
+            }
         }
     }
-}
 
-setInterval(update, 100);
+    setInterval(update, 100);
+}
